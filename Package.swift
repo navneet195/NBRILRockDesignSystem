3bdcfe9e50356269e1062a1d695cb21b330301e4
navneet195 <navneetbaldha195@gmail.com>
2024-01-27 23:07:57 +0530

    Add README file

--- Package.swift
+++ Package.swift
@@ -6,6 +6,12 @@ import PackageDescription
 let package = Package(
     name: "NBRILRockDesignSystem",
     platforms: [.iOS(.v15)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "NBRILRockDesignSystem",
+            targets: ["NBRILRockDesignSystem"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -13,7 +19,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
+        .target(
             name: "NBRILRockDesignSystem",
             dependencies: []),
         .testTarget(
